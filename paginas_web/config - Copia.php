78cf48a3eaeeeaaf61b8a430b2803f10006d6d6a
Willian Ribeiro dos Santos <126080284+will-rds@users.noreply.github.com>
2023-08-20 08:05:23 -0300

    Add files via upload

---
+++ paginas_web/config - Copia.php
@@ -0,0 +1,10 @@
+<?php
+// Configurações do banco de dados
+$db_config = [
+    'host' => 'seu_host',
+    'port' => '5432',
+    'dbname' => 'seu_db',
+    'user' => 'seu_user',
+    'password' => 'sua_senha'
+];
+
