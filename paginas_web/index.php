1ce09c4b78c2bb63efc043d4cb2a7b4621d7b74f
Willian Ribeiro dos Santos <126080284+will-rds@users.noreply.github.com>
2023-08-20 08:04:40 -0300

    Create index.php

---
+++ paginas_web/index.php
@@ -0,0 +1,68 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Fechamento Gfopag</title>
+    <style>
+        body {
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            height: 100vh;
+            margin: 0;
+            font-family: Arial, sans-serif;
+            background-color: #f0f0f0;
+        }
+
+        .container {
+            text-align: center;
+            padding: 20px;
+            border: 1px solid #ccc;
+            background-color: #fff;
+            box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
+            border-radius: 5px;
+        }
+
+        h1 {
+            margin-bottom: 10px;
+        }
+
+        p {
+            margin-bottom: 20px;
+        }
+
+        .options {
+            display: flex;
+            justify-content: center;
+        }
+
+        .options a {
+            text-decoration: none;
+            margin: 0 10px;
+        }
+
+        button {
+            padding: 10px 20px;
+            font-size: 16px;
+            background-color: #007bff;
+            color: #fff;
+            border: none;
+            border-radius: 5px;
+            cursor: pointer;
+        }
+
+        button:hover {
+            background-color: #0056b3;
+        }
+    </style>
+</head>
+<body>
+    <div class="container">
+        <h1>Fechamento Gfopag</h1>
+        <p>Selecione uma das opções abaixo:</p>
+        <div class="options">
+            <a href="servico_comum.php"><button>Serviços Comuns</button></a>
+            <a href="servico_extra.php"><button>Serviços Extras</button></a>
+        </div>
+    </div>
+</body>
+</html>
